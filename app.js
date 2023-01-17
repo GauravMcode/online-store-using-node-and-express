b08fae0a9a1f5765d55e824e6d051aa80654ffdd
Gaurav <gauravrajanju@gmail.com>
2023-01-17 20:30:01 +0530

    csrf token for security

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongodbStore = require('connect-mongodb-session')(session);  //this function execution returns a constructor function
+const csrf = require('csurf');
 
 
 const errorController = require('./controllers/error');
@@ -27,17 +28,8 @@ const store = MongodbStore({
     collection: 'sessions'
 })
 
+const protectedToken = csrf();
 
-
-// app.use((req, res, next) => {
-//     User.findById('63c2a40543bab386fba97b21')
-//         .then((user => {
-//             req.session.user = user;
-//             // console.log(user);
-//             next();
-//         }))
-//         .catch(err => console.log(err));
-// });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -46,11 +38,19 @@ app.use(
     session({ secret: 'some secret hashed value', resave: false, saveUninitialized: false, store: store })
 )
 
+app.use(protectedToken);
+
+app.use((req, res, next) => {  //these variables are passed to all the views
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+})
+
+/*the csrf token is added to session as well as passed as cookie to views along with form. now, 
+when we received next req i.e. the post request. the token in session is matched with the one we
+received. It ensures that post req is given only to views rendered by us/ */
+
 app.use(authRoutes);
-
-
-
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
