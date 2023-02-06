1205bb5e2cf5a229a099d483bd6014714f5d2c2f
Gaurav <gauravrajanju@gmail.com>
2023-02-07 02:06:50 +0530

    added @cyclic.sh/s3fs package

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('@cyclic.sh/s3fs');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -60,11 +60,11 @@ const fileFilter = (req, file, cb) => {  //to filter files for only of specific 
     }
 }
 
-// const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' }) //{flags : a} means that log data will be appended and not overriden
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' }) //{flags : a} means that log data will be appended and not overriden
 
 app.use(helmet({ crossOriginEmbedderPolicy: false }))  //adds various headers to each response to set secure response headers
 app.use(compression())  //compresses assets i.e. css js files for faster loading
-// app.use(morgan("combined", { stream: accessLogStream }))
+app.use(morgan("combined", { stream: accessLogStream }))
 app.use(
     helmet.contentSecurityPolicy({ //to set the CSP to enable access to script src of stripe
         directives: {
